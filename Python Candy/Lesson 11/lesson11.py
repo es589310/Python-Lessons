88858887754def9b16b5582e1533428d3bce3443
mr.panahov <panahovemin10@gmail.com>
2024-09-23 14:35:24 +0400

    now

---
+++ Python Candy/Lesson 11/lesson11.py
@@ -0,0 +1,30 @@
+# lesson_dict = {
+#     "ad" : "fatime",
+#     "yas" : 18,
+#     "seher" : "baki",
+#     "e-poct" : "ehaggmail.com"
+# }
+# message = {
+#     "ad" : [lesson_dict["ad"]],
+#     "yas" : [lesson_dict["yas"]],
+#     "sheher" : [lesson_dict["seher"]],
+#     "e-mail" : [lesson_dict["e-poct"]]
+# }
+
+
+# print(message)
+
+
+#Git Hub
+"""
+Git Hub
+User account açmaq
+Visual Studio Code ilə layihələri calaşdırmaq
+terminalda:
+    -git init -> layiheni initializing edir
+    -git add . -> layiheni paketleyir
+    -git commit -m "Her hansi bir soz"
+    -git branch -M master
+    -git remote add origin https://github.com/es589310/Python-Lessons.git
+    -git push -u origin master
+"""
---
+++ Python Candy/Lesson 13/lesson13.py
@@ -0,0 +1,18 @@
+# 1)ad və yaş, seçkilərdə iştirak edə bilsin
+# name = input("Adınızı qeyd edin: ")
+# age = int(input("Yaşınızı qeyd edin: "))
+
+# if age >= 18:
+#     print(f"{name}, siz seçkilərdə iştirak edə bilərsiz!")
+# elif age < 18:
+#     print(f"{name}, siz seçkilərdə iştirak edə bilməzsiniz!")
+# else:
+#     print(f"{name}, siz xarici vətəndaşsınız!")
+# #Task1 
+
+user = "bxsly23"
+password = "12345"
+
+user_name = input()
+user_password = input
+
